f44f600ca9989b5f1b663422040ff73ad3d460e8
Sera Oraha <72953661+sera-11@users.noreply.github.com>
2024-02-19 23:24:42 +0000

    provided names for routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,22 +3,24 @@ Rails.application.routes.draw do
 
   #get "/", controller: "movies", action: "index"
 
-  get "/" => "movies#index"
+  #get "/" => "movies#index"
+
+  root "movies#index"
 
   # Routes for the Movie resource:
 
   # CREATE
-  post "/movies" => "movies#create"
+  post "/movies" => "movies#create", as: :movies # movies_url and movies_path
 
-  get "/movies/new" => "movies#new"
+  get "/movies/new" => "movies#new", as :new_movie 
           
   # READ
   get "/movies" => "movies#index"
-  get "/movies/:id" => "movies#show"
+  get "/movies/:id" => "movies#show", as: :movie #renames the path to movie_path() (expect an argument)
   
   # UPDATE
   patch "/movies/:id" => "movies#update"
-  get "/movies/:id/edit" => "movies#edit"
+  get "/movies/:id/edit" => "movies#edit", as: :edit_movie 
   
   # DELETE
   delete "/movies/:id" => "movies#destroy"
